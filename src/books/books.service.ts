5f7515fa803d49c478f5679eb65b6c19d143f2cb
Paulo Sergio <pauloserg861@gmail.com>
2022-10-07 20:46:08 -0300

    criando servico de create book

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/PrismaService';
+import { CreateBookDto } from './dto/create-book.dto';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+  constructor(private primaModel: PrismaService) {}
+
+  async create(data: CreateBookDto) {
+    const bookExists = await this.primaModel.book.findFirst({
+      where: {
+        bar_code: data.bar_code,
+      },
+    });
+
+    if (bookExists) throw new Error('Esse livro já existe');
+
+    const newBook = await this.primaModel.book.create({
+      data,
+    });
+
+    return newBook;
+  }
+}
